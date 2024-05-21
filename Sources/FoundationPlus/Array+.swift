797375eca524c8a4f8aeb34c6573dd81aa2d087c
Jaewon Yun <woin2ee@gmail.com>
2024-05-21 17:35:48 +0900

    Improve comments of `index(of:)`

--- Sources/FoundationPlus/Array+.swift
+++ Sources/FoundationPlus/Array+.swift
@@ -28,11 +28,12 @@ extension Array {
 
 extension Array where Element: CaseIterable & Equatable {
 
-    /// AllCases 배열에서 주어진 열거형 값에 해당하는 index 를 반환합니다.
-    /// - Parameter element: 열거형 값
-    /// - Returns: 주어진 열거형 값에 해당하는 index
-    public func index(of element: Element) -> Int {
-        guard let index = self.firstIndex(of: element) else {
+    /// Returns the index corresponding to the given enum value in the `AllCases`.
+    ///
+    /// - Parameter enumValue: The value of enum.
+    /// - Returns: The index corresponding to the given `enumValue`.
+    public func index(of enumValue: Element) -> Int {
+        guard let index = self.firstIndex(of: enumValue) else {
             fatalError("This method was used where it was not appropriate.")
         }
         return index
